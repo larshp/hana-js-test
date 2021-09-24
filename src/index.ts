1e05813637a45a01a68e972d10d7e39791393c59
larshp <larshp@hotmail.com>
2021-09-24 14:14:40 +0200

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
+const hdb = require('hdb');
+require('dotenv').config();
+
+var client = hdb.createClient({
+  host     : process.env.DB_HOST,
+  port     : 443,
+  user     : 'DBADMIN',
+  password : process.env.DB_PASS,
+//  initializationTimeout: 150000,
+  useCesu8 : false
+});
+
+function onError(err: any) {
+  console.error('Network connection error', err);
+}
+client.on('error', onError);
+
+function onClose() {
+  console.log('Client closed');
+}
+client.on('close', onClose);
+
+function onConnect() {
+  console.log('Client connected');
+}
+client.on('connect', onConnect);
+
+function callback(foo: any, bar: any) {
+  console.dir(foo);
+  console.dir(bar);
+}
+
+client.connect(callback);
 console.dir("hello world");
